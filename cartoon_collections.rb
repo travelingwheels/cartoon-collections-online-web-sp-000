5d34e46d4c58ba2aee10b4998ff229c3c2464cb3
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-01 01:06:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 def summon_captain_planet(array)
   planeteer_calls = []
-  array.collect do|calls| planeteer_calls 
+  array.collect do|calls| planeteer_calls << {calls.capitalize
   
  
 end
