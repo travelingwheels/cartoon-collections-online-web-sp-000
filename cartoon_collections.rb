d603c5fff94d80ff69645b6bbf65f0aafc9ce72a
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-01 01:09:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ end
 def summon_captain_planet(array)
   planeteer_calls = []
   array.collect do|calls| planeteer_calls << "{calls.capitalize}!"
-  
- 
+  end
+planeteer_calls 
 end
 
 def long_planeteer_calls# code an argument here
